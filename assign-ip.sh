a756049cccf48ac72c1718f383aeb9e97d60b6ed
Adam Beneš <adam.bilovice@seznam.cz>
2026-04-09 14:06:50 +0200

    Disable NetworkManager for 192.168.7.1/24 network.

--- assign-ip.sh
+++ assign-ip.sh
@@ -7,6 +7,10 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+if command -v nmcli >/dev/null 2>&1; then
+    sudo nmcli dev set $device managed no
+fi
+
 sudo ip addr replace 192.168.7.1/24 dev $device
 echo "IP assigned to device: $device"
 exit 0
